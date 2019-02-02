63725d248b130f7720a096db0b1e6934f3b3af25
Juan Manuel Carrillo Moreno <inetshell@gmail.com>
2019-02-02 09:55:39 -0600

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,47 @@
 FROM centos:7
 MAINTAINER Manuel Carrillo "inetshell@gmail.com"
 
-ENV OPENVPN_VERSION openvpn-2.4.5
+ENV OPENVPN_VERSION="openvpn-2.4.5" \
+    DHPARAM_SIZE="1024" \
+    SCRIPT_SECURITY="3" \
+    KEEPALIVE="10 20" \
+    PROTO="tcp4-server" \
+    CIPHER="AES-256-CBC" \
+    NCP_CIPHERS="AES-256-GCM:AES-256-CBC" \
+    AUTH="SHA512" \
+    SERVER="192.168.112.0 255.255.240.0" \
+    LPORT="1194" \
+    MAX_CLIENTS="4096" \
+    COMP_LZO="yes" \
+    TOPOLOGY="subnet" \
+    SCRAMBLE="reverse"
 
 WORKDIR /tmp
 
+COPY openvpn.conf.j2 /root/openvpn.conf.j2
+COPY ${OPENVPN_VERSION}/ /tmp/${OPENVPN_VERSION}/
 COPY entrypoint.sh /entrypoint.sh
-COPY ${OPENVPN_VERSION} /tmp/${OPENVPN_VERSION}/
 
-RUN ls /tmp
-RUN ls /tmp/${OPENVPN_VERSION}/
+RUN \
+# Update YUM cache
+  yum install -y epel-release && \
+  yum update -y && \
+# Install dependencies and clean YUM cache
+  yum install -y iptables autoconf.noarch automake file gcc libtool patch quilt git make rpm-build zlib-devel pam-devel openssl openssl-devel lzo-devel lz4-devel.x86_64 net-tools cmake.x86_64 && \
+  yum clean all && \
+  cd ${OPENVPN_VERSION}/ && \
+# Build OpenVPN
+  autoreconf -i -v -f && \
+  ./configure && \
+  make && \
+  make install && \
+# Remove working directory
+  rm -rf /tmp/* && \
+# Create openvpn config directory
+  mkdir /etc/openvpn && \
+# Set permissions to entrypoint.sh
+  chmod u+x /entrypoint.sh
+
+EXPOSE 1194
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,49 @@
 #!/bin/bash -x
 OPENVPN_CONF="/etc/openvpn/server.conf"
+DHPARAM_PATH="/etc/openvpn/dhparam.pem"
 
-# Add firewall rules
+echo "Generating openvpn.conf"
+cp "/root/openvpn.conf.j2" "${OPENVPN_CONF}"
+sed -e "s:{{ SCRIPT_SECURITY }}:${SCRIPT_SECURITY}:" \
+    -e "s:{{ KEEPALIVE }}:${KEEPALIVE}:" \
+    -e "s:{{ PROTO }}:${PROTO}:" \
+    -e "s:{{ CIPHER }}:${CIPHER}:" \
+    -e "s:{{ NCP_CIPHERS }}:${NCP_CIPHERS}:" \
+    -e "s:{{ AUTH }}:${AUTH}:" \
+    -e "s:{{ SERVER }}:${SERVER}:" \
+    -e "s:{{ LPORT }}:${LPORT}:" \
+    -e "s:{{ MAX_CLIENTS }}:${MAX_CLIENTS}:" \
+    -e "s:{{ COMP_LZO }}:${COMP_LZO}:" \
+    -e "s:{{ TOPOLOGY }}:${TOPOLOGY}:" \
+    -e "s:{{ SCRAMBLE }}:${SCRAMBLE}:"
+    -i "${OPENVPN_CONF}"
+
+if [[ -f "${DHPARAM_PATH}" ]]; then
+    echo "DHPARAM file exists"
+else
+    echo "Generating DHPARAM file"
+    openssl dhparam -out "${DHPARAM_PATH}" "${DHPARAM_SIZE}"
+fi
+
+echo "Generating cert files"
+if [[ -z ${BASE64_CA_CRT} ]] || [[ -z ${BASE64_SERVER_CRT} ]] || [[ -z ${BASE64_SERVER_KEY} ]] || [[ -z ${BASE64_TLS_AUTH} ]]; then
+    echo "${BASE64_CA_CRT}" | base64 -d > "/etc/openvpn/ca.crt"
+    chmod 700 "/etc/openvpn/ca.crt"
+
+    echo "${BASE64_SERVER_CRT}" | base64 -d > "/etc/openvpn/server.crt"
+    chmod 700 "/etc/openvpn/server.crt"
+
+    echo "${BASE64_SERVER_KEY}" | base64 -d > "/etc/openvpn/server.key"
+    chmod 700 "/etc/openvpn/server.key"
+
+    echo "${BASE64_TLS_AUTH}" | base64 -d > "/etc/openvpn/tls_auth"
+    chmod 700 "/etc/openvpn/tls_auth"
+fi
+
+echo "Adding firewall rules"
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o ovpns1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i ovpns1 -o eth0 -j ACCEPT
 
-# Run OpenVPN service
+echo "Run OpenVPN service"
 /usr/local/sbin/openvpn --config "${OPENVPN_CONF}"
